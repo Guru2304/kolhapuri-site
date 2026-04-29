9d3f04469d06a13b984e1d48b74bb3478f7b1c86
Gurunath Patil <your-email@gmail.com>
2026-04-29 15:44:20 +0530

    updated website UI and categories

--- script.js
+++ script.js
@@ -4,24 +4,10 @@ function addToCart(name, price) {
   let cart = document.getElementById("cart");
 
   let li = document.createElement("li");
-  li.innerHTML = name + " - ₹" + price + " ";
+  li.innerHTML = name + " - ₹" + price;
 
-  let btn = document.createElement("button");
-  btn.innerText = "Remove";
-
-  btn.onclick = function () {
-    li.remove();
-    total -= price;
-    updateTotal();
-  };
-
-  li.appendChild(btn);
   cart.appendChild(li);
 
   total += price;
-  updateTotal();
-}
-
-function updateTotal() {
   document.getElementById("total").innerText = total;
 }
